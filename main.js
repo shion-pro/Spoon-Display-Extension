8d75fa9f99070980a43b5fa931d6717c8cfdfc1d
shion-pro <shion.programming@gmail.com>
2021-01-02 18:08:16 +0900

    ver 1.0.0

--- main.js
+++ main.js
@@ -8,15 +8,43 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     return;
 })
 
-lastMessageData = {type:"none", author:"none", content:"none", present_type:"none"};
+lastMessageData = {type:"none", author:"none", auth_img:"none", content:"none", present_type:"none"};
 
-commentCheckLoop();
+// not to lost connection
+function connectingSocket() {
+    socket.emit("chat", "connecting");
+    setTimeout("connectingSocket()", 1000);
+}
 
-function commentCheckLoop() {
+// send message
+function sendMessage(send_message) {
+    function setKeywordText(text) {
+        var el = document.getElementsByClassName("input-wrap")[0].getElementsByTagName("textarea")[0];
+        el.value = text;
+        var evt = document.createEvent("Events");
+        evt.initEvent("change", true, true);
+        el.dispatchEvent(evt);
+    }
+    document.getElementsByClassName("input-wrap")[0].getElementsByTagName("textarea")[0].focus();
+    setKeywordText(send_message);
+    $('.input-wrap').find("button").trigger("click");
+}
+
+// return from server
+socket.on("returnMessage", function(return_message){
+    if (return_message == "connecting") {
+        // console.log("connecting...");
+    } else {
+        console.log("return is:", return_message);
+        sendMessage(return_message);
+    }
+});
+
+function checkComment() {
     // check new comment
     message = $(".chat-list-item:last");
     message_classes = message.attr("class");
-    messageData = {type:"none", author:"none", content:"none", present_type:"none"};
+    messageData = {type:"none", author:"none", auth_img:"none", content:"none", present_type:"none"};
     try {
         if (message_classes.includes("guide")) {
             // spoon guide
@@ -25,51 +53,93 @@ function commentCheckLoop() {
             // enter
             author = message.find("p").text().replace("さんが入室したよ", "");
             content = "さんが入室したよ!";
-            messageData = {type:"enter", author:author, content:content, present_type:"none"};
+            messageData = {type:"enter", author:author, auth_img:"none", content:content, present_type:"none"};
         } else if (message_classes.includes("message")) {
             // comment
             author = message.find(".name").text();
+            auth_img = String(message.find(".thumbnail").find("button").find("div").attr("style")).split("\"")[1];
             content = message.find("pre").text();
-            messageData = {type:"message", author:author, content:content, present_type:"none"};
+            messageData = {type:"message", author:author, auth_img:auth_img, content:content, present_type:"none"};
+            // delete this
+            if (content == "sticker") {
+                messageData = {type:"present", author:author, auth_img:"none", content:"1 Spoon X 4", present_type:"https://static.spooncast.net/jp/stickers/basic/sticker_jp_juice/sticker_jp_juice_web.png"};
+            } else if (content == "buster") {
+                messageData = {type:"present", author:author, auth_img:"none", content:"10 buster X 4", present_type:"https://static.spooncast.net/jp/stickers/basic/sticker_jp_juice/sticker_jp_juice_web.png"};
+            } else if (content == "heart") {
+                messageData = {type:"like", author:author, auth_img:"none", content:"さんがハートを押したよ！", present_type:"none"};
+            }
+            // ==========
         } else if (message_classes.includes("combo")) {
             // combo comment
             author = lastMessageData.author;
             content = message.find("pre").text();
-            messageData = {type:"combo", author:author, content:content, present_type:"none"};
+            messageData = {type:"combo", author:author, auth_img:lastMessageData.auth_img, content:content, present_type:"none"};
+            // delete this
+            if (content == "sticker") {
+                messageData = {type:"present", author:author, auth_img:"none", content:"1 Spoon X 4", present_type:"https://static.spooncast.net/jp/stickers/basic/sticker_jp_juice/sticker_jp_juice_web.png"};
+            } else if (content == "buster") {
+                messageData = {type:"present", author:author, auth_img:"none", content:"10 buster X 4", present_type:"https://static.spooncast.net/jp/stickers/basic/sticker_jp_juice/sticker_jp_juice_web.png"};
+            } else if (content == "heart") {
+                messageData = {type:"like", author:author, auth_img:"none", content:"さんがハートを押したよ！", present_type:"none"};
+            }
+            // ==========
         } else if (message_classes.includes("like")) {
             // heart
             author = message.find("p").text().replace("さんがハートを押したよ！", "");
             content = "さんがハートを押したよ!";
-            messageData = {type:"like", author:author, content:content, present_type:"none"};
+            messageData = {type:"like", author:author, auth_img:"none", content:content, present_type:"none"};
         } else if (message_classes.includes("present")) {
             // present
             author = message.find(".name").text();
-            console.log("preそのまま", message.find("pre").text());
             content = message.find("pre").text().replace("\"", "").replace(/[\"]/g,""); // dq escape いったんこれでやってみる
-            console.log("dqエスケープver", content);
-            present_type_src = message.find(".sticker-thumbnail img").attr("src").split("/");
-            // get present_src
-            present_type = present_type_src[present_type_src.length-3] + "\\" + present_type_src[present_type_src.length-1];
-            console.log("present_type", present_type);
-            messageData = {type:"present", author:author, content:content, present_type:present_type};
-            console.log("messageData", messageData);
+            present_type_src = message.find(".sticker-thumbnail img").attr("src");
+            messageData = {type:"present", author:author, auth_img:"none", content:content, present_type:present_type_src};
         } else if (message_classes.includes("play")) {
             // voting
             author = "none";
             content = message.find("p").text();
-            messageData = {type:"play", author:author, content:content, present_type:"none"};
+            messageData = {type:"play", author:author, auth_img:"none", content:content, present_type:"none"};
         } else if (message_classes.includes("follow")) {
             // follow
         } else {
             // pass
         }
-        if (messageData.type != lastMessageData.type || messageData.author != lastMessageData.author || messageData.content != lastMessageData.content) {
+        if (messageData.type != lastMessageData.type || messageData.author != lastMessageData.author || messageData.auth_img != lastMessageData.auth_img || messageData.content != lastMessageData.content || messageData.present_type != lastMessageData.present_type) {
             console.log(messageData);
             lastMessageData = messageData;
-            socket.emit("chat", messageData);
+            my_name = $(".nickname").find("button").html();
+            if (messageData.author != my_name) {
+                socket.emit("chat", messageData);
+            }
         }
     } catch {
         // pass
     }
-    setTimeout("commentCheckLoop()", 100);
 }
+
+function startCheckComment() {
+    observer = new MutationObserver(function(){
+        checkComment();
+    });
+    comments_parent = document.getElementsByClassName("chat-list")[0];
+    config = {
+        attributes: true,
+        childList: true,
+        characterData: true,
+        subtree: true
+    };
+    observer.observe(comments_parent, config);
+};
+
+function readyChatList() {
+    comments_parent = document.getElementsByClassName("chat-list")[0];
+    if (comments_parent == undefined) {
+        setTimeout("readyChatList()", 100);
+    } else {
+        startCheckComment();
+    }
+}
+
+connectingSocket();
+
+readyChatList();
