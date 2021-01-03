b904bfd93b0c43785b474557dbb0648d6277e024
Shion <52683992+shion-pro@users.noreply.github.com>
2021-01-03 18:09:12 +0900

    Update main.js

--- main.js
+++ main.js
@@ -60,28 +60,28 @@ function checkComment() {
             auth_img = String(message.find(".thumbnail").find("button").find("div").attr("style")).split("\"")[1];
             content = message.find("pre").text();
             messageData = {type:"message", author:author, auth_img:auth_img, content:content, present_type:"none"};
-            // delete this
-            if (content == "sticker") {
-                messageData = {type:"present", author:author, auth_img:"none", content:"1 Spoon X 4", present_type:"https://static.spooncast.net/jp/stickers/basic/sticker_jp_juice/sticker_jp_juice_web.png"};
-            } else if (content == "buster") {
-                messageData = {type:"present", author:author, auth_img:"none", content:"10 buster X 4", present_type:"https://static.spooncast.net/jp/stickers/basic/sticker_jp_juice/sticker_jp_juice_web.png"};
-            } else if (content == "heart") {
-                messageData = {type:"like", author:author, auth_img:"none", content:"さんがハートを押したよ！", present_type:"none"};
-            }
+            // for test
+//             if (content == "sticker") {
+//                 messageData = {type:"present", author:author, auth_img:"none", content:"1 Spoon X 4", present_type:"https://static.spooncast.net/jp/stickers/basic/sticker_jp_juice/sticker_jp_juice_web.png"};
+//             } else if (content == "buster") {
+//                 messageData = {type:"present", author:author, auth_img:"none", content:"10 buster X 4", present_type:"https://static.spooncast.net/jp/stickers/basic/sticker_jp_juice/sticker_jp_juice_web.png"};
+//             } else if (content == "heart") {
+//                 messageData = {type:"like", author:author, auth_img:"none", content:"さんがハートを押したよ！", present_type:"none"};
+//             }
             // ==========
         } else if (message_classes.includes("combo")) {
             // combo comment
             author = lastMessageData.author;
             content = message.find("pre").text();
             messageData = {type:"combo", author:author, auth_img:lastMessageData.auth_img, content:content, present_type:"none"};
-            // delete this
-            if (content == "sticker") {
-                messageData = {type:"present", author:author, auth_img:"none", content:"1 Spoon X 4", present_type:"https://static.spooncast.net/jp/stickers/basic/sticker_jp_juice/sticker_jp_juice_web.png"};
-            } else if (content == "buster") {
-                messageData = {type:"present", author:author, auth_img:"none", content:"10 buster X 4", present_type:"https://static.spooncast.net/jp/stickers/basic/sticker_jp_juice/sticker_jp_juice_web.png"};
-            } else if (content == "heart") {
-                messageData = {type:"like", author:author, auth_img:"none", content:"さんがハートを押したよ！", present_type:"none"};
-            }
+            // for test
+//             if (content == "sticker") {
+//                 messageData = {type:"present", author:author, auth_img:"none", content:"1 Spoon X 4", present_type:"https://static.spooncast.net/jp/stickers/basic/sticker_jp_juice/sticker_jp_juice_web.png"};
+//             } else if (content == "buster") {
+//                 messageData = {type:"present", author:author, auth_img:"none", content:"10 buster X 4", present_type:"https://static.spooncast.net/jp/stickers/basic/sticker_jp_juice/sticker_jp_juice_web.png"};
+//             } else if (content == "heart") {
+//                 messageData = {type:"like", author:author, auth_img:"none", content:"さんがハートを押したよ！", present_type:"none"};
+//             }
             // ==========
         } else if (message_classes.includes("like")) {
             // heart
